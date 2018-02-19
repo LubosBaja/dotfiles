917bbb156918beb761dca51aae17cd722513238f
Lubos Baja <lubos.baja@gmail.com>
2018-02-19 08:37:35 +0100

    Add install script

---
+++ install.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+git clone --bare https://github.com/LubosBaja/dotfiles.git $HOME/.dotfiles
+
+function config{
+   /usr/bin/git --git-dir=$HOME/.dotfiles --work-tree=$HOME $@ 
+}
+mkdir -p ~/.dotfiles-backup
+config checkout
+if [$?=0]; then
+    echo "Checked out dotfiles.";
+else
+    echo "Backing up pre-existing dot files.";
+    config checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mv {} .dotfiles-backup/{}
+fi;
+config checkout
+    config config status.showUntrackedFiles no
